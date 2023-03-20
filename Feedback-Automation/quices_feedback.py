3c0cc2df842c57236ec201d21be1d6dfbceec3a4
Santiago Neusa <99107537+sneusar@users.noreply.github.com>
2023-03-20 14:31:27 -0500

    Update quices_feedback.py

--- Feedback-Automation/quices_feedback.py
+++ Feedback-Automation/quices_feedback.py
@@ -1,7 +1,3 @@
-# Program for automation of monitoring and accompaniment of quices.
-# Made by Santiago Neusa.
-# More information in https://github.com/sneusar/Computational-Thinking-NFI
-
 import os
 import sys
 import shutil
@@ -9,25 +5,25 @@ import datetime
 import subprocess
 import pandas as pd
 
-print('\nBienvenido al programa de automatización de seguimiento de quices.')
+print('\nBienvenido al programa de automatización de seguimiento de talleres.')
 
 # asking for the monitor data
 group_name = input('\nEscriba el código del grupo al que le desea realizar el seguimiento: ')
 monitor_name = input('\nIngrese su nombre: ')
-print('\nIngrese el quiz que desea revisar copiando la letra q y luego el número.\nSi ingresa \'q3\' se revisaría solo el quiz 3.')
-print('Si ingresa \'q4 q5\' se revisarían quiz 4 y 5.')
-quices_list = input('Ingrese el quiz: ').upper().split()
+print('\nIngrese el taller que desea revisar copiando la letra t y luego el número.\nSi ingresa \'t8\' se revisaría solo el taller 8.')
+workshops = input('Ingrese el taller: ').upper().split()
+print('\nLa fecha debe ir en el formato \'nombre día mes\', por ejemplo \'domingo 5 de marzo\'.')
+end_date = input('Ingrese la fecha límite de entrega del taller: ')
 
-quices = [f'Quiz 0{quiz[1]}' for quiz in quices_list]
-quices_names = ''
-for quiz in quices: quices_names += f' {quiz}'
+workshops_names = ''
+for workshop in workshops: workshops_names += f' Taller {workshop[1]}'
 
 folder_path = os.getcwd()
 file_list = os.listdir(folder_path)
-group_name_folder = f'Grupo {group_name} -{quices_names}'
+group_name_folder = f'Grupo {group_name} -{workshops_names}'
 os.makedirs(group_name_folder, exist_ok = True)
 
-feedback_txt_file = f'Mensajes Grupo {group_name} -{quices_names}.txt'
+feedback_txt_file = f'Mensajes Grupo {group_name} -{workshops_names}.txt'
 results_path = group_name_folder + '/'
 
 dataframe = pd.read_csv(os.path.join(folder_path, (group_name + '.csv')), sep = ';')
@@ -41,14 +37,19 @@ with open(feedback_txt_file, 'w', encoding='utf-8') as f:
 
     for column in dataframe.columns:
         if column == 'Nombre' or column == 'Apellido(s)' or column == 'Dirección de correo': continue
-        elif any(quiz in column for quiz in quices):
-            activity_name = column[13:-7]
-            activity_name = activity_name[:9].replace(' -', ':') + activity_name[9:]
-            activity_name = activity_name.replace(' -', '')
+        elif any(workshop in column for workshop in workshops):
+            activity_name = column.replace('Laboratorio virtual de programación:', '')
+            activity_name = activity_name.replace(' - Ejercicio - ', '')
+            activity_name = activity_name.replace(' (Real)', '')
+            activity_name = f'Taller {activity_name[1:2]} Ejercicio {activity_name[2:4]}: {activity_name[4:]}'
             dataframe = dataframe.rename(columns = {column: activity_name})
         else: del dataframe[column]
+    
+    columns_to_sort = dataframe.columns[3:]
+    sorted_columns = sorted(columns_to_sort)
+    dataframe = pd.concat([dataframe.iloc[:, :3], dataframe[sorted_columns]], axis=1)
+        
     students = {}
-
     row_counter = 3
     for index, row in dataframe.iterrows():
         name = row[0]
@@ -57,7 +58,7 @@ with open(feedback_txt_file, 'w', encoding='utf-8') as f:
         unsent_activities = []
 
         for grade in row[3:]:
-            if grade == '-' or float(grade) < 4:
+            if grade == '-' or float(grade) < 100:
                 unsent_activities.append((dataframe.columns[row_counter], grade))
             row_counter += 1
         row_counter = 3
@@ -73,20 +74,20 @@ with open(feedback_txt_file, 'w', encoding='utf-8') as f:
         for activity in data[1]:
             print(f'     {activity[0]}')
             if activity[1] == '-': print('     Nota: No enviado')
-            else: print('     Nota:', activity[1])
+            else: print(f'     Nota: {activity[1]}')
    
-        print(f'\nAsí que quería saber si tienes dudas sobre esos temas, y si es así, podemos reunirnos, de forma presencial o virtual.')
+        print(f'\nAsí que quería recordarte que la fecha de entrega es el {end_date}. Además, decirte que si tienes dudas, podemos reunirnos, de forma presencial o virtual.')
         print(f'Cordial saludo, {monitor_name}.\n\n')
-    
+        
     # creating xlsx file
+    dataframe.insert(loc = 3, column = 'Grupo', value = group_name)
     now = str(datetime.datetime.now())
-    dataframe.insert(loc = 3, column = 'Fec Reporte', value = now[:10])
-    dataframe.insert(loc = 4, column = 'Grupo', value = group_name)
+    dataframe.insert(loc = 4, column = 'Fec Reporte', value = now[:10])
     dataframe.insert(dataframe.shape[1], 'Fecha de contacto', '')
     dataframe.insert(dataframe.shape[1], 'Comentario', '')
     dataframe.insert(dataframe.shape[1], 'Hubo Asesoría', '')
 
-    feedback_xlsx_file = f'Seguimiento {feedback_txt_file[8:-4]}.xlsx'
+    feedback_xlsx_file = f'Seguimiento{feedback_txt_file[8:-4]}.xlsx'
     dataframe.to_excel((results_path + feedback_xlsx_file), index = False)
     
 
